c18e56320baa79bcd824f6c9cf999159b3dac789
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-08-21 01:59:55 +0300

    commit tree a3d8f4247baad0fe21d67a21011b70d1eadec755

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+   # getter
+   def name
+    @name
+end
 
+# setter
+def name=(input)
+    @name = input
+end
+
+# getter
+def breed
+    @breed
+end
+
+# setter
+def breed=(input)
+    @breed = input
+end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+    def name
+        @name
+    end
 
+    # setter
+    def name=(input)
+        @name = input
+    end
+
+    # getter
+    def job
+        @job
+    end
+
+    # setter
+    def job=(input)
+        @job = input
+    end
 end
